1ae739182d1d07973e6e6a899348951ac53435b0
mariabadarne <mariaba@ac.sce.ac.il>
2025-05-12 17:02:54 +0300

    create global exel

--- AutoExamSystem/AutoExamSystem___/RegisterForm.cs
+++ AutoExamSystem/AutoExamSystem___/RegisterForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClosedXML.Excel;
+using System.IO;
+
 
 namespace AutoExamSystem___
 {
@@ -50,6 +54,34 @@ namespace AutoExamSystem___
             }
 
             MessageBox.Show("הרשמה הושלמה בהצלחה!");
+
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Users.xlsx");
+
+
+       
+
+            if (!File.Exists(filePath))
+            {
+                var wb = new XLWorkbook();
+                var ws = wb.Worksheets.Add("Users");
+                ws.Cell(1, 1).Value = "Username";
+                ws.Cell(1, 2).Value = "Password";
+                ws.Cell(1, 3).Value = "ID";
+                ws.Cell(1, 4).Value = "Email";
+                wb.SaveAs(filePath);
+
+                var workbook = new XLWorkbook(filePath);
+                var worksheet = workbook.Worksheet("Users");
+
+                int lastRow = worksheet.LastRowUsed().RowNumber() + 1;
+                worksheet.Cell(lastRow, 1).Value = txtUsername.Text;
+                worksheet.Cell(lastRow, 2).Value = txtPassword.Text;
+                worksheet.Cell(lastRow, 3).Value = txtID.Text;
+                worksheet.Cell(lastRow, 4).Value = txtEmail.Text;
+
+                workbook.Save();
+                MessageBox.Show("המשתמש נשמר בהצלחה!");
+            }
         }
     }
 }
