202655cde0cade5b8be69b56e1dc9fa78679c0ba
cohavcahana <cohavca@ac.sce.ac.il>
2025-05-27 13:24:22 +0300

    create UnitTest for RegisterForm

---
+++ PRO1.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace PRO1.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PRO1.Tests/ValidationTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace PRO1.Tests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        public void ValidUsername_ShouldReturnTrue()
+        {
+            string username = "abcde1";
+            bool result = ValidationHelper.IsValidUsername(username);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void InvalidUsername_TooShort_ShouldReturnFalse()
+        {
+            string username = "a1";
+            bool result = ValidationHelper.IsValidUsername(username);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidPassword_ShouldReturnTrue()
+        {
+            string password = "Pass123!";
+            bool result = ValidationHelper.IsValidPassword(password);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void InvalidPassword_MissingSpecialChar_ShouldReturnFalse()
+        {
+            string password = "Pass1234";
+            bool result = ValidationHelper.IsValidPassword(password);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ValidEmail_ShouldReturnTrue()
+        {
+            string email = "user@example.com";
+            bool result = ValidationHelper.IsValidEmail(email);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void InvalidID_WrongLength_ShouldReturnFalse()
+        {
+            string id = "12345678";
+            bool result = ValidationHelper.IsValidID(id);
+            Assert.IsFalse(result);
+        }
+    }
+}
+
