4b27f6a6ca9dc10dff2c7c46812a038f9e5c851c
cohavcahana <cohavca@ac.sce.ac.il>
2025-06-10 15:48:26 +0300

    Translate question type options and labels to English

--- PRO1/QuestionForm.Designer.cs
+++ PRO1/QuestionForm.Designer.cs
@@ -38,19 +38,19 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(350, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.Size = new System.Drawing.Size(224, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = ":בחר סוג שאלה להוספה";
+            this.label1.Text = "Please select a question type to add";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
-            "רב-ברירה",
-            "שאלה פתוחה",
-            "שאלת נכון/לא נכון",
-            "השלמת משפטים"});
+            "Multiple Choice",
+            "Open Question",
+            "True/False",
+            "Fill in the Blanks"});
             this.comboBox1.Location = new System.Drawing.Point(327, 108);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(196, 24);
