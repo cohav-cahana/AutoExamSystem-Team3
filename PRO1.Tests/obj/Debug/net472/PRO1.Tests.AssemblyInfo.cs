969d9cfd84b7f929f95d7756cd8dc8eca4355a33
cohavcahana <cohavca@ac.sce.ac.il>
2025-06-10 23:51:31 +0300

    mergh??

--- PRO1.Tests/obj/Debug/net472/PRO1.Tests.AssemblyInfo.cs
+++ PRO1.Tests/obj/Debug/net472/PRO1.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRO1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc02b6b567f02ccae76ab23cba2c16dc1ace78b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d24d62c56aaa8efd05a1e1e9b3d620f2ccfd4a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRO1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRO1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
